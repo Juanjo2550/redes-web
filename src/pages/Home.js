54729061ac1a74b0c85fa4f4214cb1724892bfa7
juanjo2550 <juan.jaramillo02@epn.edu.ec>
2020-07-23 00:48:26 -0500

    react

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {GlobalStyle} from './styles';
+
+const Home = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Redes - cp</h1>
+      <iframe src="https://giphy.com/embed/eMmJJR6pLL4Nkt8eZJ" width="480" height="254" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ascii-art-text-death-dealer-eMmJJR6pLL4Nkt8eZJ">via GIPHY</a></p>
+    </>
+  );
+};
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry:'./src/App.js',
+  output: {
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [['@babel/plugin-transform-runtime', { regenerator: true }]],
+          },
+        },
+      },
+    ],
+  },
+};
