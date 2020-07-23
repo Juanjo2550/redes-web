ba0a220f9b68eb1cdfb0b4fe3733174777564af1
juanjo2550 <juan.jaramillo02@epn.edu.ec>
2020-07-23 00:46:08 -0500

    build script

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const chalk = require("chalk");
 const app = express();
 
-app.use(express.static("./public/"));
+app.use(express.static("./dist/"));
 
 app.listen(80, () => {
   console.log(chalk.bgGreen("Servidor creado en el puerto 80"));
